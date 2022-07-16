ec90452d441bcf7dc546d7d226b03fb42b99a348
huyanhai <810839700@qq.com>
2022-07-16 23:55:12 +0800

    feat: add

--- src/views/Plan.vue
+++ src/views/Plan.vue
@@ -1,62 +1,73 @@
 <template>
   <div class="plan">
-    <div class="container">
-      <div class="step1">
-        <h3 class="title animate__animated animate__fadeInDown">“微生素”小微金融生命体系</h3>
-        <div class="box">
-          <div class="anim-b">
-            <img class="r a-icon" src="@/assets/plan-s1-2.png" alt="" />
-            <img class="s animate__animated animate__zoomIn" src="@/assets/plan-s1-1.png" alt="" srcset="" />
-            <span class="t t1 animate__animated a-icon2 animate__delay-1-1">多元产品体系</span>
-            <span class="t t2 animate__animated a-icon3 animate__delay-1-2">以客户为中心<br />的运营体系</span>
-            <span class="t t3 animate__animated a-icon3 animate__delay-1-3">标准高效的<br />交付新体系</span>
-            <span class="t t4 animate__animated a-icon3 animate__delay-1-4">数字化小微智<br />能创新体系</span>
-            <span class="t t5 animate__animated a-icon3 animate__delay-1-5">数据信贷的风险识<br />别与量化风控体系</span>
-          </div>
-          <div class="scroll animate__animated animate__fadeInDown animate__infinite" style="animation-duration: 2s">
-            <img src="@/assets/plan-scroll.png" alt="" srcset="" />
-            <p>向下滑动</p>
-          </div>
-        </div>
-      </div>
-      <div class="step2 wow animate__animated animate__fadeInDown" data-wow-offset="100">
-        <h3 class="title">“毕昇”数字小微赋能平台</h3>
-        <div class="items">
-          <div class="item" v-for="(item, index) in list" :key="index">
-            <img :src="item.icon" alt="" srcset="" />
-            <span>{{ item.text }}</span>
-            <p>{{ item.desc }}</p>
+    <swiper :direction="'vertical'" class="swiper-box" :mousewheel="true" :modules="[Mousewheel]" v-on:slide-change="onSlideChange">
+      <swiper-slide>
+        <div class="container">
+          <div class="step1">
+            <h3 class="title animate__animated animate__fadeInDown">“微生素”小微金融生命体系</h3>
+            <div class="box">
+              <div class="anim-b">
+                <img class="r a-icon" src="@/assets/plan-s1-2.png" alt="" />
+                <img class="s animate__animated animate__zoomIn" src="@/assets/plan-s1-1.png" alt="" srcset="" />
+                <span class="t t1 animate__animated a-icon2 animate__delay-1-1">多元产品体系</span>
+                <span class="t t2 animate__animated a-icon3 animate__delay-1-2">以客户为中心<br />的运营体系</span>
+                <span class="t t3 animate__animated a-icon3 animate__delay-1-3">标准高效的<br />交付新体系</span>
+                <span class="t t4 animate__animated a-icon3 animate__delay-1-4">数字化小微智<br />能创新体系</span>
+                <span class="t t5 animate__animated a-icon3 animate__delay-1-5">数据信贷的风险识<br />别与量化风控体系</span>
+              </div>
+              <div class="scroll animate__animated animate__fadeInDown animate__infinite" style="animation-duration: 2s">
+                <img src="@/assets/plan-scroll.png" alt="" srcset="" />
+                <p>向下滑动</p>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-      <div class="step3">
-        <h3 class="title wow animate__animated animate__fadeInDown">小微中台业务驱动与管理</h3>
-        <div class="items">
-          <img class="wow animate__animated animate__zoomIn" src="@/assets/plan-s3-1.png" data-wow-delay="0.5s" alt="" srcset="" />
-          <span class="t t1 wow animate__animated animate__fadeInDown6" data-wow-delay="1.5s">小微产品运营<br />监控管理</span>
-          <span class="t t2 wow animate__animated animate__fadeInDown6" data-wow-delay="2s">小微产品设计<br />定义与管理</span>
-          <span class="t t3 wow animate__animated animate__fadeInDown6" data-wow-delay="2.5s">小微客户<br />分析与管理</span>
-          <span class="t t4 wow animate__animated animate__fadeInDown6" data-wow-delay="3s">营销管理，决策审核<br />与审批(含自动决策)，<br />贷后管理</span>
-        </div>
-      </div>
-      <div class="step4">
-        <h3 class="title wow animate__animated animate__fadeInDown">“Cornerstone·基石”业务</h3>
-        <div class="box">
-          <img class="s1 wow animate__animated animate__zoomIn" data-wow-delay="0.5s" src="@/assets/plan-s4-1.png" alt="" />
-          <img class="s2 wow a-icon1" src="@/assets/plan-s4-2.png" data-wow-delay="1s" alt="" />
-          <div class="icon wow animate__animated animate__fadeInDown6" :data-wow-delay="`${1.5 + index * 0.1}s`" :class="'i' + (index + 1)" v-for="(item, index) in list1" :key="index">
-            <img :src="item.icon" />
-            <p>{{ item.text }}</p>
+      </swiper-slide>
+      <swiper-slide>
+        <div class="container">
+          <!-- <div class="step2 wow animate__animated animate__fadeInDown">
+            <h3 class="title">“毕昇”数字小微赋能平台</h3>
+            <div class="items">
+              <div class="item" v-for="(item, index) in list" :key="index">
+                <img :src="item.icon" alt="" srcset="" />
+                <span>{{ item.text }}</span>
+                <p>{{ item.desc }}</p>
+              </div>
+            </div>
+          </div> -->
+          <div class="step3">
+            <h3 class="title wow animate__animated animate__fadeInDown">小微中台业务驱动与管理</h3>
+            <!-- <div class="items">
+              <img class="wow animate__animated animate__zoomIn" src="@/assets/plan-s3-1.png" data-wow-delay="0.5s" alt="" srcset="" />
+              <span class="t wow t1 animate__animated animate__fadeInDown6" data-wow-delay="1.5s">小微产品运营<br />监控管理</span>
+              <span class="t wow t2 animate__animated animate__fadeInDown6" data-wow-delay="2s">小微产品设计<br />定义与管理</span>
+              <span class="t wow t3 animate__animated animate__fadeInDown6" data-wow-delay="2.5s">小微客户<br />分析与管理</span>
+              <span class="t wow t4 animate__animated animate__fadeInDown6" data-wow-delay="3s">营销管理，决策审核<br />与审批(含自动决策)，<br />贷后管理</span>
+            </div> -->
           </div>
         </div>
-      </div>
-    </div>
-    <user-footer />
+      </swiper-slide>
+      <swiper-slide>
+        <div class="container">
+          <div class="step4">
+            <h3 class="title wow animate__animated animate__fadeInDown">“Cornerstone·基石”业务</h3>
+            <div class="box">
+              <img class="s1 animate__animated animate__zoomIn" data-wow-delay="0.5s" src="@/assets/plan-s4-1.png" alt="" />
+              <img class="s2 a-icon1" src="@/assets/plan-s4-2.png" data-wow-delay="1s" alt="" />
+              <div class="icon animate__animated animate__fadeInDown6" :data-wow-delay="`${1.5 + index * 0.1}s`" :class="'i' + (index + 1)" v-for="(item, index) in list1" :key="index">
+                <img :src="item.icon" />
+                <p>{{ item.text }}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <user-footer />
+      </swiper-slide>
+    </swiper>
   </div>
 </template>
 <script setup>
 import UserFooter from "@/components/Footer.vue";
-// import { WOW } from "wowjs";
 import s1 from "@/assets/plan-s2-1.png";
 import s2 from "@/assets/plan-s2-2.png";
 import s3 from "@/assets/plan-s2-3.png";
@@ -67,7 +78,11 @@ import c3 from "@/assets/plan-s4-33.png";
 import c4 from "@/assets/plan-s4-34.png";
 import c5 from "@/assets/plan-s4-35.png";
 
-import { ref, onMounted } from "vue";
+import { ref } from "vue";
+import { Mousewheel } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/vue";
+import "swiper/css";
+import "swiper/css/effect-fade";
 
 const list = ref([
   {
@@ -110,24 +125,16 @@ const list1 = ref([
   },
 ]);
 
-onMounted(() => {
-  var wow = new WOW({
-    boxClass: "wow", // animated element css class (default is wow)
-    animateClass: "animated", // animation css class (default is animated)
-    offset: 0, // distance to the element when triggering the animation (default is 0)
-    mobile: true, // trigger animations on mobile devices (default is true)
-    live: true, // act on asynchronously loaded content (default is true)
-    callback: function (box) {
-      // the callback is fired every time an animation is started
-      // the argument that is passed in is the DOM node being animated
-    },
-    scrollContainer: null, // optional scroll container selector, otherwise use window
-  });
-  wow.init();
-});
+const onSlideChange = () => {
+  console.log("change");
+};
 </script>
 
 <style lang="scss" scoped>
+.swiper-box {
+  width: 100%;
+  height: 100%;
+}
 .a-icon {
   animation: animal 10s infinite linear;
   -webkit-transform-origin: center center;
@@ -148,7 +155,9 @@ onMounted(() => {
 
 .plan {
   background: url("@/assets/plan-bg.png") no-repeat #0054ff;
-  background-size: auto 100%;
+  background-size: cover;
+  width: 100%;
+  height: 100%;
   .container {
     .step1 {
       padding-top: 160px;
