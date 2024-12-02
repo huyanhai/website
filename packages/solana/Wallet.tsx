96905cd5d8094e1a1cfab415481911b0d6206fb9
Huyanhai <810839700@qq.com>
2024-12-02 17:31:59 +0800

    test: test

--- packages/solana/Wallet.tsx
+++ packages/solana/Wallet.tsx
@@ -1,24 +1,41 @@
 'use client';
+import {
+  LAMPORTS_PER_SOL,
+  Transaction,
+  PublicKey,
+  Keypair
+} from '@solana/web3.js';
 import { AnchorProvider, Program, BN } from '@project-serum/anchor';
 import { useAnchorWallet, useWallet } from '@solana/wallet-adapter-react';
 
-import {
-  Connection,
-  Keypair,
-  clusterApiUrl,
-  SystemProgram
-} from '@solana/web3.js';
+import { Connection, clusterApiUrl, SystemProgram } from '@solana/web3.js';
+import * as bs58 from 'bs58';
 
-import React, { useMemo } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 
 import idl from './idl.json';
 
+const newAccountKp = Keypair.generate();
+
 const WalletCom = () => {
   const wallet = useAnchorWallet();
-  const { connected, connect, disconnect, select, wallets, publicKey } =
-    useWallet();
+  const {
+    connected,
+    connect,
+    disconnect,
+    select,
+    wallets,
+    publicKey,
+    sendTransaction
+  } = useWallet();
 
+  const [balance, setBalance] = useState(0);
   const connection = new Connection(clusterApiUrl('devnet'));
+
+  const getBalance = async () => {
+    const data = await connection.getBalance(publicKey!);
+    setBalance(data / LAMPORTS_PER_SOL);
+  };
 
   const connectWallet = async () => {
     try {
@@ -29,49 +46,77 @@ const WalletCom = () => {
     }
   };
 
-  const getProvider = () => {
-    if (!wallet) return null;
-
+  const provider = useMemo(() => {
+    wallet && getBalance();
     return new AnchorProvider(
       connection,
-      wallet,
+      wallet!,
       AnchorProvider.defaultOptions()
     );
+  }, [wallet]);
+
+  const program = useMemo(() => {
+    return new Program(
+      idl as any,
+      '67LWCuERY8GKJfahid4fonb3G39HkAerbnSP6xDkd6QF',
+      provider!
+    );
+  }, [provider]);
+
+  const getMsg = async () => {
+    const [pd] = PublicKey.findProgramAddressSync(
+      [Buffer.from('new_account')],
+      program?.programId!
+    );
+
+    try {
+      const account: any = await program.account.newAccount.fetch(pd);
+      console.log('account', account.data.toString());
+    } catch (error) {
+      await program.methods
+        .initialize(new BN(120))
+        .accounts({
+          newAccount: pd,
+          signer: wallet?.publicKey,
+          systemProgram: SystemProgram.programId
+        })
+        .rpc();
+      const account = await program.account.newAccount.fetch(pd);
+      console.log('account', account, pd.toBase58());
+    }
   };
 
-  const provider = getProvider()!;
+  const send = async () => {
+    const transaction = new Transaction();
+    const action = SystemProgram.transfer({
+      fromPubkey: wallet?.publicKey!,
+      toPubkey: new PublicKey('8jk53tHXqeoWyBDQXAC7bz5dk5t3p8cvN3LUJTgpPcPx'),
+      lamports: 0.1 * LAMPORTS_PER_SOL
+    });
 
-  const getMsg = () => {
-    if (provider) {
-      const newAccountKp = new Keypair();
-      const program = new Program(
-        idl as any,
-        '67LWCuERY8GKJfahid4fonb3G39HkAerbnSP6xDkd6QF',
-        provider
-      );
+    transaction.add(action);
+    const data = await sendTransaction(transaction, connection);
+    console.log('data is ', data);
+  };
 
-      const test = async () => {
-        const txHash = await program.methods
-          .initialize(new BN(42))
-          .accounts({
-            newAccount: newAccountKp.publicKey,
-            signer: publicKey,
-            systemProgram: SystemProgram.programId
-          })
-          .signers([newAccountKp])
-          .rpc();
+  const add = async () => {
+    const [pd] = PublicKey.findProgramAddressSync(
+      [Buffer.from('new_account')],
+      program?.programId!
+    );
 
-        await connection.sendEncodedTransaction(txHash);
+    console.log('执行add', pd);
 
-        // Fetch the created account
-        const newAccount = await program.account.newAccount.fetch(
-          newAccountKp.publicKey
-        );
-
-        console.log(newAccount);
-      };
-      test();
-    }
+    await program.methods
+      .add()
+      .accounts({
+        newAccount: pd,
+        signer: wallet?.publicKey,
+        systemProgram: SystemProgram.programId
+      })
+      .rpc();
+    const account = await program.account.newAccount.fetch(pd);
+    console.log('account', account, pd.toBase58());
   };
 
   const InstalledWallet = useMemo(() => {
@@ -81,6 +126,7 @@ const WalletCom = () => {
   return (
     <>
       <div style={{ color: 'white' }}>Wallet is {publicKey?.toBase58()}</div>
+      <div style={{ color: 'white' }}>余额:{balance}</div>
       <button onClick={connectWallet} style={{ background: 'yellow' }}>
         链接钱包
       </button>
@@ -90,6 +136,12 @@ const WalletCom = () => {
 
       <button onClick={getMsg} style={{ background: 'red' }}>
         获取消息
+      </button>
+      <button onClick={add} style={{ background: 'yellow' }}>
+        新增
+      </button>
+      <button onClick={send} style={{ background: 'blue' }}>
+        转账
       </button>
     </>
   );
